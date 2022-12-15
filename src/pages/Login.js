bdb031dd81ced2704f812841d6d72714fcf77f63
Chiksinafees <nafeesahmed01.na@gmail.com>
2022-12-15 16:20:37 +0530

    Implementing Login ( task 15 )

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -19,37 +19,45 @@ const Login = () => {
     e.preventDefault();
     setIsLoading(true);
 
+    let url;
     if (isLogin) {
-      //mmmm
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCENs_kbPG5MUBwwErYSVMnS9ZTwdVtRHs";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCENs_kbPG5MUBwwErYSVMnS9ZTwdVtRHs",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: email,
-            password: password,
-            returnSecureToken: true,
-          }),
-          Headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCENs_kbPG5MUBwwErYSVMnS9ZTwdVtRHs";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      }),
+      Headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
         setIsLoading(false);
         if (res.ok) {
-          //...
+          return res.json();
         } else {
           return res.json().then((data) => {
             let errorMessage = "Authentication failed";
             if (data.error.message) {
               errorMessage = data.error.message;
             }
-            alert(errorMessage);
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
       });
-    }
     setEmail("");
     setPassword("");
   };
